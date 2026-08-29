a0d920408fc4fc4b27f8dd509eef4c58984fbc1b
noahvsb <noahvansteenbrugge@gmail.com>
2026-08-29 15:56:17 +0200

    part 2/2 of leaderboard scraper

--- lib/leaderboard/util.ts
+++ lib/leaderboard/util.ts
@@ -1,4 +1,4 @@
-import { checkResponse, Options } from "@/util";
+import { checkResponse, compressCode, Options } from "@/util";
 import { writeFile } from "node:fs/promises";
 
 const leaderboardUrl = (_id: number): string => `https://www.freeriderhd.com/track_api/load_leaderboard`;
@@ -57,33 +57,71 @@ type LeaderboardEntry = {
   };
 }
 
-export async function getUidsAndPlaces(id: number): Promise<UserData[]> {
+type RaceEntry = {
+  user: {
+    u_id: number;
+    u_name: string;
+    d_name: string;
+    img_url_small: string;
+    cosmetics: unknown;
+  };
+  race: {
+    code: string;
+    vehicle: string;
+    desktop: boolean;
+    run_ticks: number;
+  };
+}
+
+export async function getUsers(id: number): Promise<Map<number, UserData>> {
   const res = await fetch(leaderboardUrl(id), leaderboardBody(id));
   checkResponse(res);
   const data = (await res.json()).track_leaderboard;
-  return data
-    .filter((entry: LeaderboardEntry) => entry.user)
-    .map((entry: LeaderboardEntry) => ({
-      id: entry.u_id,
-      name: entry.user.d_name,
-      place: entry.place,
-      time: entry.run_time,
-    }));
+  return new Map(
+    data
+      .filter((entry: LeaderboardEntry) => entry.user)
+      .map((entry: LeaderboardEntry): [number, UserData] => [
+        entry.u_id,
+        {
+          id: entry.u_id,
+          name: entry.user.d_name,
+          place: entry.place,
+          time: entry.run_time,
+        },
+      ])
+  );
 }
 
 export async function fetchLeaderboard(id: number, options: Options): Promise<LeaderboardData> {
-  const uidsPlaces = await getUidsAndPlaces(id);
+  const users = await getUsers(id);
 
-  const uids = uidsPlaces.map(({id}) => id);
-  
-  const res = await fetch(racesUrl(id), racesBody(id, uids));
+  const res = await fetch(racesUrl(id), racesBody(id, [ ...users.keys() ]));
   checkResponse(res);
 
-  const data = (await res.json()).data;
+  const data: RaceEntry[] = (await res.json()).data;
 
-  // TODO
+  const leaderboard = await Promise.all(
+    data.map(async (entry) => {
+      const user = users.get(entry.user.u_id)!;
 
-  return <any>{};
+      let maybeCompressed = entry.race.code;
+      if (options.compress) {
+        maybeCompressed = await compressCode(maybeCompressed);
+      }
+
+      const race: RaceData = {
+        vehicle: entry.race.vehicle,
+        desktop: entry.race.desktop,
+        runTicks: entry.race.run_ticks,
+        code: maybeCompressed,
+        compressed: Boolean(options.compress),
+      };
+
+      return { user, race };
+    })
+  );
+
+  return { id, leaderboard };
 }
 
 export async function writeLeaderboardData(data: LeaderboardData, options: Options): Promise<void> {
@@ -92,4 +130,5 @@ export async function writeLeaderboardData(data: LeaderboardData, options: Optio
 
 export async function scrapeLeaderboard(id: number, options: Options): Promise<void> {
   const leaderboard = await fetchLeaderboard(id, options);
+  await writeLeaderboardData(leaderboard, options);
 }
